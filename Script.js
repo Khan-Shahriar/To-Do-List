babfaa4e8160259509d528a1be4c9b6b4c11d01a
Khan-Shahriar <ikhanshahriar@gmail.com>
2023-11-24 05:10:20 +0600

    Done

---
+++ Script.js
@@ -0,0 +1,34 @@
+const inputBox = document.getElementById("input-box");
+const listCont = document.getElementById("list-cont");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Wright Something Please");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listCont.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData()
+}
+listCont.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listCont.innerHTML);
+}
+function showTask(){
+    listCont.innerHTML = localStorage.getItem("data");
+}
+showTask();
